e498359c91200d8855f6d66e0ad0c94ae144e27e
gauri0304 <gaurimhatre2001@gmail.com>
2025-07-14 16:04:04 +0530

    updated files

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-# Importing essential libraries
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -42,49 +40,4 @@ def predict():
     return render_template('Predict.html')
 
 if __name__ == '__main__':
-=======
-# Importing essential libraries
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
-
-# Load the Random Forest CLassifier model
-filename = 'heart-disease-prediction-random-forest-model.pkl'
-model = pickle.load(open(filename, 'rb'))
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-	return render_template('Predict.html')
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        age = int(request.form['age'])
-        sex = int(request.form.get('sex'))
-        cp = int(request.form.get('cp'))
-        trestbps = int(request.form['trestbps'])
-        chol = int(request.form['chol'])
-        fbs = int(request.form.get('fbs'))
-        restecg = int(request.form['restecg'])
-        thalach = int(request.form['thalach'])
-        exang = int(request.form.get('exang'))
-        oldpeak = float(request.form['oldpeak'])
-        slope = int(request.form.get('slope'))
-        ca = int(request.form['ca'])
-        thal = int(request.form.get('thal'))
-
-        data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg,
-                          thalach, exang, oldpeak, slope, ca, thal]])
-        my_prediction = model.predict(data)
-
-        result_text = "You may have heart disease." if my_prediction[0] == 1 else "You are unlikely to have heart disease."
-        
-        return render_template('Predict.html', prediction_text=result_text)
-
-    return render_template('Predict.html')
-
-if __name__ == '__main__':
->>>>>>> 4f2b27e41e6a2f8ad1c64224f8c0dd219a9d86b2
 	app.run(debug=True)
